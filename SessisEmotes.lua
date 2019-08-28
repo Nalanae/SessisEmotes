491475b15cbb5f151883e592013ee20b0882aa9f
Nalanae <wrmenkveld@gmail.com>
2019-08-29 00:06:29 +0200

    Initial commit

---
+++ SessisEmotes.lua
@@ -0,0 +1,90 @@
+SESSISEMOTES = {}
+
+SESSISEMOTES["sessA"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessA:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessAngel"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessAngel:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessAww"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessAww:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessBad"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessBad:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessBits"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessBits:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessCarry"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessCarry:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessCosy"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessCosy:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessDerp"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessDerp:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessDerp1"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessDerp1:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessDerp2"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessDerp2:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessDerp3"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessDerp3:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessDerp4"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessDerp4:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessG"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessG:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessGift"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessGift:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessHey"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessHey:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessHug"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessHug:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessHype"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessHype:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessHypers"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessHypers:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessKkappqa"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessKkappqa:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessL"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessL:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessLick"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessLick:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessLul"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessLul:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessLurk"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessLurk:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessNom"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessNom:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessPink"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessPink:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessPog"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessPog:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessRB"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessRB:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessRekt"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessRekt:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessRigged"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessRigged:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessRip"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessRip:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessSad"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessSad:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessScared"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessScared:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessShy"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessShy:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessTip"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessTip:20:20:0:0:20:20:0:20:0:20\124t"
+SESSISEMOTES["sessYay"] = "\124TInterface/AddOns/SessisEmotes/emotes/sessYay:20:20:0:0:20:20:0:20:0:20\124t"
+
+function replaceSessisEmotes(message, t, replaceStr, emote)
+    local tp = t
+    if emote then
+        tp = string.sub(t, 1, 1).."%"..string.sub(t, 2, 2)
+    end
+
+    message = string.gsub(message, "^"..tp.."$", replaceStr) --solo emote
+    message = string.gsub(message, "^"..tp.."(%s)", replaceStr.."%1") --emote at beginning with trailing space
+    message = string.gsub(message, "(%s)"..tp.."(%s)", "%1"..replaceStr.."%2") --emote with leading and trailing space
+    message = string.gsub(message, "(%s)"..tp.."$", "%1"..replaceStr) --emote at end with leading space
+
+    return message
+end
+
+-- event function
+function onChatMessage(self, event, message, ...)
+    for t in string.gmatch(message, "%S+") do
+        if SESSISEMOTES[t] then
+            local possibleEmote = string.sub(t, 2, 2) == ")" or string.sub(t, 2, 2) == "(" -- handle robot face emotes (this solution is borrowed from TwitchEmotes addon)
+            message = replaceSessisEmotes(message, t, SESSISEMOTES[t], possibleEmote)
+        end
+    end
+
+    return false, message, ...
+end
+
+-- supported chat types which the code will apply to
+local CHAT_TYPES = {
+ "AFK",
+ "BATTLEGROUND_LEADER",
+ "BATTLEGROUND",
+ "BN_WHISPER",
+ "BN_WHISPER_INFORM",
+ "CHANNEL",
+ "DND",
+ "EMOTE",
+ "GUILD",
+ "OFFICER",
+ "PARTY_LEADER",
+ "PARTY",
+ "RAID_LEADER",
+ "RAID_WARNING",
+ "RAID",
+ "SAY",
+ "WHISPER",
+ "WHISPER_INFORM",
+ "YELL",
+}
+
+for _, type in pairs(CHAT_TYPES) do
+    ChatFrame_AddMessageEventFilter("CHAT_MSG_" .. type, onChatMessage)
+end
